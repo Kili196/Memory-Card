274d160ab54c7a4075f5c848dff3f107f715058c
kili196 <kilian.baier16@gmail.com>
2025-04-06 18:45:44 +0200

    add style folder
    add header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
+import Header from "./components/Header";
 import "./global.css";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <div className="heading"> Memory game</div>
-    </>
+    <div className="memory-game">
+      <Header />
+    </div>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <header className="heading"> dsad </header>;
+};
+
+export default Header;
