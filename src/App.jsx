44eae28fda2b36672eef2222df2bdf305707dec1
kili196 <kilian.baier16@gmail.com>
2025-04-06 18:11:37 +0200

    add css reset
    add purple linear gradient

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
+import "./global.css";
 
 function App() {
   return (
     <>
-      <div className="cloud"> Memory game</div>
+      <div className=""> Memory game</div>
     </>
   );
 }
