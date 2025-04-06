536cf87e9a3aa0d5b20a7dc9a45600deab1212b5
kili196 <kilian.baier16@gmail.com>
2025-04-06 18:13:22 +0200

    add card background color to global root

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./global.css";
+import "./App.css";
 
 function App() {
   return (
     <>
-      <div className=""> Memory game</div>
+      <div className="heading"> Memory game</div>
     </>
   );
 }
