2d826638f3367b54451e194bb04425521901663c
kili196 <kilian.baier16@gmail.com>
2025-04-07 20:05:34 +0200

    add card styling add cardOptions remove bug in global css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
 import { useState } from "react";
 
 import Header from "./components/Header";
+import Card from "./components/Card";
 
 function App() {
+  const cardOptions = ["🧛‍♀️", "🧞", "👨‍🚀", "🐸", "🧌", "🧙‍♂️", "🦄", "🐉"];
+
   return (
     <div className="memory-game__container">
       <Header />
+      <div className="memory-game__card-container">
+        {" "}
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+const Card = () => {
+  return (
+    <>
+      <div className="memory-game__card memory-game__card--content">🐸</div>
+    </>
+  );
+};
+
+export default Card;
